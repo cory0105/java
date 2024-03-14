8ca178d3dce93f937d23ccae1a7ec5fc7ac1ef28
cory0105 <161662652+cory0105@users.noreply.github.com>
2024-03-14 17:32:46 +0900

    0314

---
+++ studyJava/src/Study/Class/_0314_상속/Object/hashcode/t.java
@@ -0,0 +1,12 @@
+package Study.Class._0314_상속.Object.hashcode;
+
+class aa{
+
+}
+public class t {
+    public static void main(String[] args) {
+        aa a = new aa();
+        aa a2 = new aa();
+        System.out.println(a);
+    }
+}
